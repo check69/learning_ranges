276f022d3746524d551d8c0318a174c09a5ca603
check69 <antonio.checa.bustos@gmail.com>
2021-09-20 19:07:03 +0100

    Improving example

--- learning_ranges/Source.cpp
+++ learning_ranges/Source.cpp
@@ -1,14 +1,23 @@
 #include<ranges>
 #include <stdexcept>
+#include<iostream>
 #include<vector>
 
 using namespace std;
-namespace view = ranges::views;
 
 
 int main()
 {
-    std::vector<int> vec{1, 2, 3, 4};
-    auto my_view = vec | view::reverse;
+    std::vector<int> vec{1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    auto my_view = vec
+        | std::ranges::views::filter([](int i) { return i % 2; })
+        | std::ranges::views::transform([](int i) { return i * i; });
+
+    for (auto elem : my_view)
+    {
+        std::cout << elem << std::endl;
+    }
+
 	return 0;
 }
